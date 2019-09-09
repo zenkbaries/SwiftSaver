143499de71f0d9e8f7b0ffce6d3c060232e8692f
John Towery <kbaries@gmail.com>
2019-09-08 20:58:42 -0600

    Initial Commit

---
+++ SwiftSaver/SwiftSaverView.h
@@ -0,0 +1,13 @@
+//
+//  SwiftSaverView.h
+//  SwiftSaver
+//
+//  Created by John Towery on 9/8/19.
+//  Copyright © 2019 KellyBoy. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SwiftSaverView : ScreenSaverView
+
+@end
---
+++ SwiftSaver/SwiftSaverView.m
@@ -0,0 +1,52 @@
+//
+//  SwiftSaverView.m
+//  SwiftSaver
+//
+//  Created by John Towery on 9/8/19.
+//  Copyright © 2019 KellyBoy. All rights reserved.
+//
+
+#import "SwiftSaverView.h"
+
+@implementation SwiftSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
